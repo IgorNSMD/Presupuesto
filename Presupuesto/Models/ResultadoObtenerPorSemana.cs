52d79542aeda578d4c851a71db16a3645eff3ca2
IgorNSMD <insanmartind@gmail.com>
2023-02-19 12:14:06 -0300

    Capitulo 156

--- Presupuesto/Models/ResultadoObtenerPorSemana.cs
+++ Presupuesto/Models/ResultadoObtenerPorSemana.cs
@@ -7,6 +7,14 @@
 
         public TipoOperacion TipoOperacionId { get; set; }
 
+        public decimal Ingresos { get; set; }
+
+        public decimal Gastos { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
 
     }
 }
