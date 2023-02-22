4aab224c3a8cb8dd2e5f7547863e0b2d8c139a94
IgorNSMD <insanmartind@gmail.com>
2023-02-22 10:14:22 -0300

    Capitulo 171

---
+++ Presupuesto/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presupuesto.Models;
+
+namespace Presupuesto.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+
+            return RedirectToAction("Index", "Transacciones");
+        }
+    }
+}
---
+++ Presupuesto/Models/RegistroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presupuesto.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido ")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {1} es requerido ")]
+        public string Password { get; set; }
+    }
+}
