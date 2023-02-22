c5f1316e7e38ac022b989eef271b6503718202cc
IgorNSMD <insanmartind@gmail.com>
2023-02-22 18:09:18 -0300

    Capitulo 178

---
+++ Presupuesto/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presupuesto.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido ")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo ")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "El campo {1} es requerido ")]
+        public string Password { get; set; }
+
+        public bool Recuerdame { get; set; }
+    }
+}
